d181b3e1fc225bbca42583555acadc3407ebac61
Ivan Fedorov <Ivan_Fedorov@epam.com>
2017-10-29 14:14:49 +0300

    Moved WEB-INF folder to webapp Changed H2DB to in-memory type Fixed path if db.properties Removed unused jar

--- src/main/java/com/epam/students/dao/ConnectionPool.java
+++ src/main/java/com/epam/students/dao/ConnectionPool.java
@@ -1,12 +1,19 @@
 package com.epam.students.dao;
 
 import org.h2.jdbcx.JdbcConnectionPool;
+import org.h2.tools.RunScript;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.sql.Connection;
 import java.util.Properties;
 
 public class ConnectionPool {
+
+    private static final ClassLoader classLoader = ConnectionPool.class.getClassLoader();
     private static JdbcConnectionPool connectionPool;
 
     public static synchronized JdbcConnectionPool getConnectionPool() {
@@ -14,8 +21,7 @@ public class ConnectionPool {
             Properties prop = new Properties();
             String filename = "db.properties";
 
-            InputStream inputStream =
-                    ConnectionPool.class.getClassLoader().getResourceAsStream(filename);
+            InputStream inputStream = classLoader.getResourceAsStream(filename);
             try {
                 prop.load(inputStream);
             } catch (IOException e) {
@@ -26,9 +32,20 @@ public class ConnectionPool {
             String user = prop.getProperty("user");
             String password = prop.getProperty("password");
 
-            connectionPool = JdbcConnectionPool
-                    .create("jdbc:h2:file:." + url + ";IFEXISTS=true", user, password);
+            connectionPool = JdbcConnectionPool.create(url, user, password);
+
+            executeSchema("db_schema.sql", connectionPool);
         }
         return connectionPool;
+    }
+
+    private static void executeSchema(String filename, JdbcConnectionPool connectionPool) {
+        try (Connection conn = connectionPool.getConnection()) {
+            URL resource = classLoader.getResource(filename);
+            File file = new File(resource.toURI());
+            RunScript.execute(conn, new FileReader(file));
+        } catch (Exception ex) {
+            throw new RuntimeException("Failed to execute script");
+        }
     }
 }
