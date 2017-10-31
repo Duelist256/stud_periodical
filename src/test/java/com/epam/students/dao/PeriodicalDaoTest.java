e9660c12b903a1a440c19ac117e28645016eae8c
Ivan Seliukov <Ivan_Seliukov@epam.com>
2017-10-31 16:04:58 +0300

    Change test for getAll() and delete useless tests

--- src/test/java/com/epam/students/dao/PeriodicalDaoTest.java
+++ src/test/java/com/epam/students/dao/PeriodicalDaoTest.java
@@ -1,48 +1,18 @@
 package com.epam.students.dao;
 
 import com.epam.students.model.Periodical;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
 
 public class PeriodicalDaoTest {
 
-    private PeriodicalDao periodicalDao;
-    private List<Periodical> periodicals;
-
-    @Before
-    public void setPeriodicalDaoAndGetAllPeriodicals() {
-        periodicalDao = new PeriodicalDao();
-        periodicals = periodicalDao.getAll();
-        assertNotNull(periodicals);
-    }
-
     @Test
     public void checkSize() {
-        assertEquals(periodicals.size(), 20);
-    }
-
-    @Test
-    public void checkIfNotNull() {
-        for (Periodical periodical : periodicals) {
-            assertNotNull(periodical);
-        }
-    }
-
-    @Test
-    public void checkFields() {
-        int index = 1;
-        for (Periodical periodical : periodicals) {
-            assertTrue(periodical.getId() == index++);
-            assertTrue(periodical.getTitle().startsWith("Title"));
-            assertTrue(periodical.getDescription().startsWith("Description"));
-            assertTrue(periodical.getPublisher().startsWith("publisher"));
-            assertTrue(periodical.getGenre().startsWith("genre"));
-            assertTrue(periodical.getImgPath().equals("defaultpath"));
-            assertTrue(Integer.parseInt(periodical.getPrice()) >= 100);
-        }
+        PeriodicalDao periodicalDao = new PeriodicalDao();
+        List<Periodical> periodicals = periodicalDao.getAll();
+        assertFalse(periodicals.isEmpty());
     }
 }
