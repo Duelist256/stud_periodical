1f995c48a569a7b9b0b5f8abbb8a0b03f11c27f6
Dmitrii_Prikhozhaev <Dmitrii_Prikhozhaev@epam.com>
2017-11-03 16:47:34 +0300

    404.html access servlet created

---
+++ src/main/java/com/epam/students/servlet/pageaccess/Servlet404.java
@@ -0,0 +1,16 @@
+package com.epam.students.servlet.pageaccess;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Servlet404", urlPatterns = "/404")
+public class Servlet404 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("404.html").forward(req, resp);
+    }
+}
