d017a068f5ddb584290fd29b908ffac611893d2f
Dmitrii_Prikhozhaev <Dmitrii_Prikhozhaev@epam.com>
2017-11-03 05:23:47 +0300

    servlet500 implemented

--- src/main/java/com/epam/students/servlet/pageaccess/Servlet500.java
+++ src/main/java/com/epam/students/servlet/pageaccess/Servlet500.java
@@ -1,4 +1,17 @@
 package com.epam.students.servlet.pageaccess;
 
-public class Servlet500 {
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Servlet500", urlPatterns = "/500")
+public class Servlet500 extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("500.html").forward(req, resp);
+    }
 }
