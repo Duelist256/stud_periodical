4ba1ef8a399ff0ab7905f5d5de287b125052ac56
Dmitrii_Prikhozhaev <Dmitrii_Prikhozhaev@epam.com>
2017-11-03 22:36:22 +0300

    ErrorHandler implemented

---
+++ src/main/java/com/epam/students/servlet/ErrorHandler.java
@@ -0,0 +1,34 @@
+package com.epam.students.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ErrorHandler", urlPatterns = "/ErrorHandler")
+public class ErrorHandler extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        handle(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        handle(req, resp);
+    }
+
+    private void handle(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Integer statusCode = (Integer) req.getAttribute("javax.servlet.error.status_code");
+
+        switch (statusCode){
+            case 403:
+                req.getRequestDispatcher("/403").forward(req, resp); break;
+            case 404:
+                req.getRequestDispatcher("/404").forward(req, resp); break;
+            default:
+                req.getRequestDispatcher("/500").forward(req, resp); break;
+        }
+    }
+}
