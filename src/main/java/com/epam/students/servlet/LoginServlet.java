b094e7687db098c9da7676fe11d5784d1fe15e1d
Ivan Seliukov <Ivan_Seliukov@epam.com>
2017-11-06 01:46:02 +0300

    Refactoring

--- src/main/java/com/epam/students/servlet/LoginServlet.java
+++ src/main/java/com/epam/students/servlet/LoginServlet.java
@@ -49,16 +49,15 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         if (getLanguage().equals("en")) {
             setLanguage("ru");
-            req.getSession().setAttribute("language", getLanguage());
-            req.getServletContext().getRequestDispatcher("/login.jsp").forward(req, resp);
         } else {
             setLanguage("en");
-            req.getSession().setAttribute("language", getLanguage());
-            req.getServletContext().getRequestDispatcher("/login.jsp").forward(req, resp);
         }
 
+        req.getSession().setAttribute("language", getLanguage());
+        req.getServletContext().getRequestDispatcher("/login.jsp").forward(req, resp);
     }
 
 
