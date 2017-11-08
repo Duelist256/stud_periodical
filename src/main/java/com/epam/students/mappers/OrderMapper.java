7bb012edb363ba17791ab7b00f59ca273f93e66a
Dmitrii_Prikhozhaev <Dmitrii_Prikhozhaev@epam.com>
2017-11-08 17:54:21 +0300

    OrderMapper documented

--- src/main/java/com/epam/students/mappers/OrderMapper.java
+++ src/main/java/com/epam/students/mappers/OrderMapper.java
@@ -6,6 +6,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class OrderMapper {
+    /**
+     * Maps ResultSet object gotten from database into Order object.
+     * We can see Builder pattern here and its kinda nice.
+     *
+     * @param rs to base Order object on.
+     * @return Order object based on result set.
+     * @throws SQLException from result set data access methods.
+     */
     public static Order mapRow(ResultSet rs) throws SQLException {
         Order order = Order.newBuilder()
                 .id(rs.getInt("id"))
