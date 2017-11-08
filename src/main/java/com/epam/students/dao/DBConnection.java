06b8c99d13ca6f387a3f510a2cd9268361b2fe4f
Dmitrii_Prikhozhaev <Dmitrii_Prikhozhaev@epam.com>
2017-11-08 17:18:13 +0300

    OrderDao documented

--- src/main/java/com/epam/students/dao/DBConnection.java
+++ src/main/java/com/epam/students/dao/DBConnection.java
@@ -6,10 +6,18 @@ import org.h2.jdbcx.JdbcConnectionPool;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+/**
+ * Class to provide static access to connection from connection pool.
+ */
 public class DBConnection {
 
     final static Logger logger = Logger.getLogger(DBConnection.class);
 
+    /**
+     * Method to provide static access to connection from connection pool.
+     *
+     * @return connection for database from pool.
+     */
     public static Connection getConnection() {
         JdbcConnectionPool cp = ConnectionPool.getConnectionPool();
         try {
